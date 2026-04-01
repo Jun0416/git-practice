e450a73008c75278f107d906d019b852e9566fc2
junyong <doctor030416@gmail.com>
2026-04-01 16:30:27 +0000

    system report

---
+++ system_report.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+DATE=$(date)
+echo "=== 시스템 리포트 ===" >> ~/git-practice/report.txt
+echo "날짜: $DATE" >> ~/git-practice/report.txt
+echo "사용자: Junyong" >> ~/git-practice/report.txt
+echo "디스크: $(df -h / | tail -1)" >> ~/git-practice/report.txt
+echo "메모리: $(free -h | grep Mem)" >> ~/git-practice/report.txt
+echo "-------------------" >> ~/git-practice/report.txt
