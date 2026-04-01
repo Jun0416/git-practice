81207a6ab9f6db3aa75a53a23065d68158cdd001
junyong <doctor030416@gmail.com>
2026-04-01 16:42:11 +0000

    auto report

--- system_report.sh
+++ system_report.sh
@@ -6,3 +6,6 @@ echo "사용자: Junyong" >> ~/git-practice/report.txt
 echo "디스크: $(df -h / | tail -1)" >> ~/git-practice/report.txt
 echo "메모리: $(free -h | grep Mem)" >> ~/git-practice/report.txt
 echo "-------------------" >> ~/git-practice/report.txt
+git add .
+git commit -m "auto report"
+git push origin main
