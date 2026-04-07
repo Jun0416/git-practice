f0838f4bcc1738b35db56205959445892ca533f9
junyong <doctor030416@gmail.com>
2026-04-07 04:25:19 +0000

    new one

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request
+import sqlite3
+app = Flask(__name__)
+
+def get_db():
+    conn = sqlite3.connect('books.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/books')
+def get_books():
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM books')
+    books = cursor.fetchall()
+    return str([dict(book) for book in books])
+
+@app.route('/books/add', methods=['POST'])
+def add_book():
+    title = request.json['title']
+    author = request.json['author']
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO books (title, author) VALUES (?, ?)", (title, author))
+    conn.commit()
+    return '추가 성공!'
+
+@app.route('/books/delete/<id>', methods=['DELETE'])
+def delete_book(id):
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM books WHERE id = ?", (id,))
+    conn.commit()
+    return '삭제 성공!'
+
+@app.route('/books/update/<id>', methods=['PUT'])
+def update_book(id):
+    title = request.json['title']
+    author = request.json['author']
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("UPDATE books SET title=?, author=? WHERE id=?", (title, author, id))
+    conn.commit()
+    return '수정 성공!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
